4c4ce8248ba5756f73e3ff11fac67d6944ac423c
AmirHmn <amir.hosein6979@gmail.com>
2022-08-22 15:40:28 +0430

    config settings for signals in apps.py

--- chat/signals.py
+++ chat/signals.py
@@ -2,9 +2,10 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from .models import Member
+from chat_app import settings
 
 
-@receiver(post_save, sender=Member)
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_member(sender, instance, created, **kwargs):
     if created:
         Member.objects.create(user=instance)
