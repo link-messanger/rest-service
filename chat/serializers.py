60a3bee826ee32a6490b54d831ce2fbf21ad497b
AmirHmn <amir.hosein6979@gmail.com>
2022-08-22 15:41:22 +0430

    create view to retrieving specific CharRoom

--- chat/serializers.py
+++ chat/serializers.py
@@ -5,10 +5,14 @@ from chat.models import ChatRoom
 
 class ChatRoomSerializer(serializers.ModelSerializer):
     messages = serializers.SerializerMethodField()
+    members = serializers.SerializerMethodField()
 
     def get_messages(self, obj):
-        return obj.messages.all()
+        return obj.message.all()
+
+    def get_members(self, obj):
+        return obj.member.all()
 
     class Meta:
         model = ChatRoom
-        fields = ['name', 'messages']
+        fields = ['name', 'messages', 'members']
--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.generics import RetrieveAPIView, ListAPIView
 
-# Create your views here.
+from chat.models import ChatRoom
+from chat.serializers import ChatRoomSerializer
+
+
+class ChatRoomView(RetrieveAPIView):
+
+    def get_queryset(self):
+        name = self.kwargs['name']
+        return ChatRoom.objects.filter(name=name)
+
+    serializer_class = ChatRoomSerializer
+    lookup_field = 'name'
