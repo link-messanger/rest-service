05d30133510ab404aaa5ee451df35f59f8ff02dc
AmirHmn <amir.hosein6979@gmail.com>
2022-08-24 18:43:41 +0430

    update settings

--- chat_app/settings/prod.py
+++ chat_app/settings/prod.py
@@ -10,13 +10,8 @@ DEBUG = False
 ALLOWED_HOSTS = ['amir-chatapp.herokuapp.com']
 
 DATABASES = {
-  'default': {
-    'ENGINE': 'django.db.backends.mysql',
-    'NAME': os.environ.get('DB_NAME'),
-    'HOST': os.environ.get('DB_HOST'),
-    'PORT': os.environ.get('DB_PORT'),
-    'USER': os.environ.get('DB_USER'),
-    'PASSWORD': os.environ.get('DB_PASSWORD'),
-    'OPTIONS': {'ssl': {'ca': os.environ.get('MYSQL_ATTR_SSL_CA')}}
-  }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        "DATABASE_URL": os.environ['DATABASE_URL'],
+    }
 }
