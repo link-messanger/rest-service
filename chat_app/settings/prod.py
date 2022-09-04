a414bc9d12c64fce2e0160c3ae76f3258d432d8f
AmirHMN <amir.hosein6979@gmail.com>
2022-09-03 21:25:05 -0400

    update settings and requirement.txt

--- chat_app/settings/prod.py
+++ chat_app/settings/prod.py
@@ -1,6 +1,5 @@
 import os
 
-import dj_database_url
 
 from .settings import *
 
@@ -9,10 +8,17 @@ SECRET_KEY = os.environ['SECRET_KEY']
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['amir-chatapp.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 DATABASES = {
-    'default': dj_database_url.config()
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_NAME'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': 'db',
+        'PORT': 5432,
+    }
 }
 
 SIMPLE_JWT = {
